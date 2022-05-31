acce0624477d1395278d4c7d0520f96d04a752e6
Dae314 <discord@dae314>
2022-05-30 16:35:54 -1000

    Fix index.js to import token correctly

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // Require the necessary discord.js classes
 const { Client, Intents } = require('discord.js');
-const { token } = process.env.DISCORD_TOKEN;
+const dotenv = require('dotenv');
+dotenv.config();
+const token = process.env.DISCORD_TOKEN;
 
 // Create a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
