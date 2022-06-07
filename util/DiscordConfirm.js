d3c76a8ab44c60b9f61aa95ec56ac02a46ebca5e
Dae314 <discord@dae314>
2022-06-06 23:44:50 -1000

    Add timeout response to DiscordConfirm

--- util/DiscordConfirm.js
+++ util/DiscordConfirm.js
@@ -1,6 +1,6 @@
 const { MessageActionRow, MessageButton } = require('discord.js');
 
-module.exports = async function DiscordConfirm(interaction, confirmText, confirmCallback) {
+module.exports = async function DiscordConfirm(interaction, confirmText, confirmCallback, timeout=15000) {
 	const row = new MessageActionRow()
 		.addComponents(
 			new MessageButton()
@@ -10,15 +10,19 @@ module.exports = async function DiscordConfirm(interaction, confirmText, confirm
 		);
 
 	await interaction.reply({ content: confirmText, components: [row], ephemeral: true });
+	const confirmTimeout = setTimeout(async () => {
+		await interaction.editReply({content: 'No response means no work! ╰(▔∀▔)╯', components: [], ephemeral: true});
+	}, timeout);
 
 	const filter = (buttonInteraction) => {
 		return interaction.user.id === buttonInteraction.user.id;
 	}
 
-	const collector = interaction.channel.createMessageComponentCollector({filter, max: 1, time: 15000});
+	const collector = interaction.channel.createMessageComponentCollector({filter, max: 1, time: timeout});
 
 	collector.on('collect', async interaction => {
 		if(interaction.customId === 'confirmButton') {
+			clearTimeout(confirmTimeout);
 			await confirmCallback(interaction);
 		}
 	});
