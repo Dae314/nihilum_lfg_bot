ed7c967004c10aa956a1efa71ff176c56230dd12
Dae314 <discord@dae314>
2022-06-06 23:44:20 -1000

    Add lfg-admin-delete-group command

---
+++ commands/lfg-admin-delete-group.js
@@ -0,0 +1,42 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { Permissions } = require('discord.js');
+const DiscordConfirm = require('../util/DiscordConfirm.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('lfg-admin-delete-group')
+		.setDescription('Admin command: delete a group')
+		.addIntegerOption(option =>
+			option.setName('group')
+				.setDescription('The ID of the group that will be deleted')
+				.setRequired(true)),
+	async execute(interaction, db, client) {
+		const isAdmin = interaction.member.permissions.has(Permissions.FLAGS.ADMINISTRATOR);
+		const groupID = interaction.options.getInteger('group');
+		const groupEntry = await db.getGroup(groupID);
+		if(!isAdmin) {
+			await interaction.reply(`No way you just tried to run an admin command without being an admin, you naughty user you! (╬ Ò ‸ Ó)`);
+			return;
+		}
+		if(!groupEntry) {
+			await interaction.reply({content: `Sorry I couldn't find that group anywhere. ┐(￣ヘ￣;)┌`, ephemeral: true});
+			return;
+		}
+		try {
+			await DiscordConfirm(interaction,
+				`Are you sure you want to delete "${groupEntry.name}"?`,
+				async (buttonInteraction) => {
+					const members = await db.getGroupMembers(groupEntry.id);
+					for(const member of members) {
+						await db.rmMember(member.id, groupEntry.id);
+					}
+					await db.rmGroup(groupEntry.id);
+					await buttonInteraction.update({content: `That group is gone now!`, components: [], ephemeral: true});
+				}
+			)
+		} catch(err) {
+			await interaction.reply({content: `I wasn't able to delete that group ๐·°(⋟﹏⋞)°·๐ Check the console log.`, ephemeral: true});
+			console.log(err);
+		}
+	},
+};
