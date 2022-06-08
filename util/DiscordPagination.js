dcbf130d6dfe2b5e343cbaa0b532171fea2b5fb3
Dae314 <discord@dae314>
2022-06-07 19:33:09 -1000

    Convert to custom DiscordPagination

--- util/DiscordPagination.js
+++ util/DiscordPagination.js
@@ -1,16 +1,54 @@
-const paginationEmbed = require('discordjs-button-pagination');
-const { MessageButton } = require('discord.js');
+const { MessageActionRow, MessageButton } = require('discord.js');
 
-module.exports = function DiscordPagination(interaction, pages) {
-	const buttonList = [
-		new MessageButton()
-			.setCustomId('prevButton')
-			.setLabel('Previous')
-			.setStyle('DANGER'),
-		new MessageButton()
-			.setCustomId('nextButton')
-			.setLabel('Next')
-			.setStyle('SUCCESS'),
-	]
-	paginationEmbed(interaction, pages, buttonList);
+module.exports = async function DiscordPagination(interaction, pages, timeout=120000) {
+	const row = new MessageActionRow()
+		.addComponents(
+			new MessageButton()
+				.setCustomId('prevButton')
+				.setLabel('Previous')
+				.setStyle('DANGER'),
+			new MessageButton()
+				.setCustomId('nextButton')
+				.setLabel('Next')
+				.setStyle('SUCCESS'),
+		);
+
+	let visibleItem = 0;
+
+	let page = pages[visibleItem].setFooter({ text: `${visibleItem + 1}/${pages.length}`});
+	await interaction.reply({ embeds: [page], components: [row] });
+		
+	let interactTimeout = setTimeout(async () => {
+		await interaction.editReply({components: []});
+	}, timeout);
+
+	const filter = (buttonInteraction) => {
+		return interaction.user.id === buttonInteraction.user.id;
+	}
+
+	const collector = interaction.channel.createMessageComponentCollector({filter, time: timeout});
+
+	collector.on('collect', async buttonInteraction => {
+		if(buttonInteraction.customId === 'prevButton') {
+			await buttonInteraction.deferUpdate();
+			clearTimeout(interactTimeout);
+			collector.resetTimer();
+			interactTimeout = setTimeout(async () => {
+				await interaction.editReply({components: []});
+			}, timeout);
+			if(--visibleItem < 0) visibleItem = pages.length - 1;
+			let page = pages[visibleItem].setFooter({ text: `${visibleItem + 1}/${pages.length}`});
+			await buttonInteraction.editReply({ embeds: [page], components: [row], ephemeral: true});
+		} else if(buttonInteraction.customId === 'nextButton') {
+			await buttonInteraction.deferUpdate();
+			clearTimeout(interactTimeout);
+			collector.resetTimer();
+			interactTimeout = setTimeout(async () => {
+				await interaction.editReply({components: []});
+			}, timeout);
+			if(++visibleItem >= pages.length) visibleItem = 0;
+			let page = pages[visibleItem].setFooter({ text: `${visibleItem + 1}/${pages.length}`});
+			await buttonInteraction.editReply({ embeds: [page], components: [row], ephemeral: true});
+		}
+	});
 }
