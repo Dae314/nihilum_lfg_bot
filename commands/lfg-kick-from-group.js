7423e253ebf61e3b27041bc5b6de2deb0567b7d1
Dae314 <discord@dae314>
2022-06-08 21:28:38 -1000

    Make all error feedback in lfg-kick-from-group ephemeral

--- commands/lfg-kick-from-group.js
+++ commands/lfg-kick-from-group.js
@@ -26,7 +26,7 @@ module.exports = {
 			ownerEntry = await db.getUserbyUsername(ownerName);
 		}
 		if(!groupEntry) {
-			await interaction.reply(`Sorry I couldn't find that group anywhere. ┐(￣ヘ￣;)┌`);
+			await interaction.reply({content: `Sorry I couldn't find that group anywhere. ┐(￣ヘ￣;)┌`, ephemeral: true});
 			return;
 		}
 		if(!targetUserEntry) {
@@ -35,27 +35,27 @@ module.exports = {
 		}
 		const isOwner = await db.userIsGroupOwner(ownerEntry.id, groupEntry.id);
 		if(!isOwner) {
-			await interaction.reply(`You can't kick someone from a group you didn't make! ☆⌒(> _ <)`);
+			await interaction.reply({content: `You can't kick someone from a group you didn't make! ☆⌒(> _ <)`, ephemeral: true});
 			return;
 		}
 		const targetIsOwner = await db.userIsGroupOwner(targetUserEntry.id, groupEntry.id);
 		if(targetIsOwner) {
-			await interaction.reply(`Woah woah hold up. You can't kick the group owner. YOU ARE THE GROUP OWNER! If you really want to leave, just use \`/lfg-leave-group group:${groupEntry.id}\``);
+			await interaction.reply({content: `Woah woah hold up. You can't kick the group owner. YOU ARE THE GROUP OWNER! If you really want to leave, just use \`/lfg-leave-group group:${groupEntry.id}\``, ephemeral: true});
 			return;
 		}
 		const inGroup = await db.userIsInGroup(targetUserEntry.id, groupEntry.id);
 		if(!inGroup) {
-			await interaction.reply(`Well the good news is... ${targetUserEntry.username} isn't in your group! So... (─‿‿─)`);
+			await interaction.reply({content: `Well the good news is... ${targetUserEntry.username} isn't in your group! So... (─‿‿─)`, ephemeral: true});
 			return;
 		}
 		try {
 			const userPrompt = await DiscordConfirm(interaction, `Are you sure you want to kick "${targetUserEntry.username}" from "${groupEntry.name}"?`);
 			if(userPrompt.confirmed) {
 				await db.rmMember(targetUserEntry.id, groupEntry.id);
-				await interaction.editReply({content: `Bye ${targetUserEntry.username}!`, components: [], ephemeral: false});
+				await interaction.editReply({content: `Bye ${targetUserEntry.username}!`, components: [], ephemeral: true});
 			}
 		} catch(err) {
-			await interaction.reply(`Something went wrong when I tried to kick that user from your group ๐·°(⋟﹏⋞)°·๐ Please report this to your admins. ;-;`);
+			await interaction.reply({content: `Something went wrong when I tried to kick that user from your group ๐·°(⋟﹏⋞)°·๐ Please report this to your admins. ;-;`, ephemeral: true});
 			console.log(err);
 		}
 	},
